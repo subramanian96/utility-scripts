10a9f105cce416e2b54766e5af7f319104932097
subramanian96 <subramanian@atsgen.com>
2020-07-23 19:55:59 +0530

    Update go installation script.

--- install_go.sh
+++ install_go.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "Installing Go"
+export SCRIPT_DIR=$(realpath $(dirname $0))
+pushd SCRIPT_DIR
 wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export GOPATH=\$HOME/go" >> ~/.bashrc
 source ~/.bashrc
+rm -rf go1.13.4.linux-amd64.tar.gz
+popd
 
