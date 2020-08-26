28bb74c4e2ca2254ac8366149962e9fb245aa504
subramanian96 <subramanian@atsgen.com>
2020-08-26 14:32:29 +0530

    Commenting out CNI

--- install_k8s.sh
+++ install_k8s.sh
@@ -16,5 +16,8 @@ kubeadm init
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# If in case you need a basic CNI uncomment the following command 
+# kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
